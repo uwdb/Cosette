d87ee018729e4f879b16febd200288509d110e8c
Shumo Chu <stechu@users.noreply.github.com>
2016-12-27 15:59:30 -0800

    Update .dir-locals.el

--- hott/.dir-locals.el
+++ hott/.dir-locals.el
@@ -2,11 +2,10 @@
                         (setq coq-prog-name "/usr/local/bin/hoqtop")
                         (set (make-local-variable 'coq-prog-args)
                            (list "-emacs-U" "-R"
-                           	 (replace-regexp-in-string "^/docker:[^:]+:" ""
+                             (replace-regexp-in-string "^/\\(\\(ssh\\|docker\\):[^:|]+[:|]\\)+" ""
                                (expand-file-name
                                  "./.build"
                                    (file-name-directory
                                      (let ((d (dir-locals-find-file ".")))
                                        (if (stringp d) d (car d))))))
                              "Cosette")))))))
-          
