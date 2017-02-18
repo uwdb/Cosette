016b1bc59ee7e86b997aad056d6b2cefe8bc88e5
stechu <shumo.chu@gmail.com>
2017-02-18 00:04:47 -0800

    clean index coq code

--- hott/optimizations/Index.v
+++ hott/optimizations/Index.v
@@ -12,22 +12,17 @@ Module Index (T : Types) (S : Schemas T) (R : Relations T S)  (A : Aggregators T
   Import T S R A.
   Module SQL_TSRA := SQL T S R A.
   Import SQL_TSRA.
-
-  Definition Index {s t0 t1} (R: SQL empty s) (k: Column t0 s) (ic: Column t1 s) :=
-    SELECT2 (variable (right⋅k)), (variable (right⋅ic)) FROM1 R.
-
-  Definition isUnique {s τ} (k: Column τ s) (R: SQL empty s) :=
-    forall t, denoteSQL R tt t ->
-         denoteSQL (SELECT1 right⋅k FROM1 R) tt (⟦k⟧ t) = Unit.
   
   Definition isKey {s t} (k: Column t s) (R: SQL empty s) :=
-    forall t1 t2, denoteSQL R tt t1 -> denoteSQL R tt t2 -> ⟦k⟧ t1 = ⟦k⟧ t2 -> t1 = t2.
-
-  Definition isKey2 {s t} (k: Column t s) (R: SQL empty s) :=
+    (forall t1 t2, denoteSQL R tt t1 -> denoteSQL R tt t2 -> ⟦k⟧ t1 = ⟦k⟧ t2 -> t1 = t2)
+    /\
     ⟦ empty ⊢ R : _ ⟧ =
     ⟦ empty ⊢ ((project (right⋅left)) (FROM R, R
                 WHERE equal (variable (right⋅left⋅k))
                 (variable (right⋅right⋅k)))): _ ⟧.
+  
+  Definition Index {s t0 t1} (R: SQL empty s) (k: Column t0 s) (ic: Column t1 s) :=
+    SELECT2 (variable (right⋅k)), (variable (right⋅ic)) FROM1 R.
 
   Lemma sum_pair_split' {A B C}:
     {ab: A * B & C ab} <~> {a:A & {b:B & C (a, b)}}.
@@ -69,75 +64,10 @@ Module Index (T : Types) (S : Schemas T) (R : Relations T S)  (A : Aggregators T
       reflexivity.
     + reflexivity.
   Qed.
-
-  Lemma equiv_prod_sigma_nested {A B C D}:
-    {a: A & {b:B & C a b * D a}} <~> {a:A & {b:B & C a b} * D a}.
-  Admitted.
-
-  Lemma equiv_prod_sigma_assoc {A B C D}:
-    {a: A & B a * C a * D a} <~> {a: A & B a * (C a * D a)}.
-  Admitted.
-
-  Lemma equiv_prod_unit {A B}:
-    (A -> (B = Unit)) ->  (A * B <~> A).
-  Proof.
-    intros h.
-    simple refine (BuildEquiv _ _ _ _). {
-      intros [a b].
-      exact a. }
-    simple refine (BuildIsEquiv _ _ _ _ _ _ _). {
-      intro a.
-      (*try equiv_hprop_allpath *)
-      rewrite (h a).
-      exact (a, tt). }
-    - unfold Sect.
-      intros.
-      unfold Overture.internal_paths_rew_r.
-  Abort.
   
-  Lemma isKeyProof: forall s t (k:Column t s) (R: SQL empty s),
-      isUnique k R -> isKey2 k R.
-  Proof.
-    intros s t k R h.
-    unfold isKey2.
-    simpl.
-    by_extensionality g.
-    by_extensionality t'.    
-    unfold isKey in h.
-    match goal with
-    |  |- _ = ∑ a, @?C a  => rewrite (path_universe_uncurried (@sum_pair_split' (Tuple s) (Tuple s) C))
-    end.
-    cbn.
-    rewrite (path_universe_uncurried (equiv_prod_sigma_nested)).
-    rewrite <- (path_universe_uncurried eqSum).
-    rewrite (path_universe_uncurried (equiv_sigma_prod_symm _ _ _)).
-    rewrite (path_universe_uncurried equiv_prod_sigma_assoc).
-    rewrite <- (path_universe_uncurried (equiv_prod_sigma _ _ _)).
-    unfold isUnique in h.
-    cbn in h.
-    specialize (h t').
-    match goal with
-    | |- _ = ?A * ?B => assert (A -> B) as h1 end.
-    { rewrite (unit_eq g).
-      intro h2.
-      specialize (h h2).
-      exists t'.
-      refine (h2, _).
-      reflexivity. }
-    { symmetry.
-      apply path_universe_uncurried.
-      rewrite (path_universe_uncurried (equiv_prod_symm _ _)).
-      match goal with
-      | |- ?A * ?B <~> _ => assert (IsHProp A) as h3 end.
-      admit.
-      apply hprop_prod_l'.
-      exact h1.
-  Abort.
-    
   Definition IntroProj: Type.
     refine (forall r (R: SQL empty r) t0 (l: constant t0)
-              (a: Column t0 r) t1 (k: Column t1 r) (ik: isKey k R)
-              (ik2: isKey2 k R), _ ).
+              (a: Column t0 r) t1 (k: Column t1 r) (ik: isKey k R), _ ).
     pose (Index R k a) as I.
     pose (empty ++ (singleton t1 ++ singleton t0 ++ empty) ++ r) as qs.
     pose (@variable _ qs (right⋅left⋅right⋅left⋅star)) as ia.
@@ -195,9 +125,9 @@ Module Index (T : Types) (S : Schemas T) (R : Relations T S)  (A : Aggregators T
     rewrite (path_universe_uncurried (equiv_sigma_prod_symm _ _ _)).
     repeat rewrite (path_universe_uncurried (sum_prod_assoc)).
     rewrite (path_universe_uncurried (equiv_prod_sigma_r _ _ _)).
-    unfold isKey2 in ik2.
+    (* unfold isKey2 in ik2. *)
     match goal with
-    | |- ?A * _ * _ * _ = _ =>  assert (IsHProp A) as h1 end. {
+    | |- ?A * _ * _ * _ = _ =>  assert (IsHProp A) as h1 end.  {
       apply ishprop_sigma_disjoint.
       intros x y h1 h2.
       destruct x as [x1 [x2 x3]].
@@ -222,6 +152,7 @@ Module Index (T : Types) (S : Schemas T) (R : Relations T S)  (A : Aggregators T
       apply hprop_prod_l'.
       intros [eq [ra rb]].
       unfold isKey in ik.
+      destruct ik as [ik ik2].
       rewrite (unit_eq g) in *.
       exact (ik ta tb ra rb eq). }
     rewrite <- h2.
@@ -257,6 +188,7 @@ Module Index (T : Types) (S : Schemas T) (R : Relations T S)  (A : Aggregators T
       apply pair_f_eq in e5.
       destruct e5 as [e5 e6].
       unfold isKey in ik.
+      destruct ik as [ik ik2].
       rewrite (unit_eq g) in *.
       rewrite e4 in e2.
       symmetry in e2.
@@ -292,8 +224,7 @@ Module Index (T : Types) (S : Schemas T) (R : Relations T S)  (A : Aggregators T
 
   Definition IndexTransProj: Type.
     refine (forall r (R: SQL empty r) t0 (l: constant t0)
-              (a: Column t0 r) t1 (k: Column t1 r) (ik: isKey k R)
-              (ik2: isKey2 k R), _ ).
+              (a: Column t0 r) t1 (k: Column t1 r) (ik: isKey k R),  _ ).
     refine ( ⟦ empty ⊢ (project (right⋅right)
                          (FROM R, R
                           WHERE and (equal (variable (right⋅left⋅a)) (constantExpr l))
@@ -318,6 +249,7 @@ Module Index (T : Types) (S : Schemas T) (R : Relations T S)  (A : Aggregators T
     apply prod_eq_assoc.
     intros h.
     unfold isKey in ik.
+    destruct ik as [ik ik2].
     destruct h as [[h1 h2] [h3 h4]].
     apply ik.
     + rewrite <- (unit_eq g).
@@ -339,8 +271,7 @@ Module Index (T : Types) (S : Schemas T) (R : Relations T S)  (A : Aggregators T
    *)
   Definition IndexExample0: Type.
     refine (forall r (R: SQL empty r) t0 (l: constant t0)
-              (a: Column t0 r) t1 (k: Column t1 r) (ik: isKey k R)
-              (ik2: isKey2 k R), _).
+              (a: Column t0 r) t1 (k: Column t1 r) (ik: isKey k R), _).
     pose (Index R k a) as I.
     pose (empty ++ (singleton t1 ++ singleton t0 ++ empty) ++ r) as qs.
     pose (@variable _ qs (right⋅left⋅right⋅left⋅star)) as ia.
@@ -366,11 +297,12 @@ Module Index (T : Types) (S : Schemas T) (R : Relations T S)  (A : Aggregators T
     unfold IndexExample0.
     unfold Index.
     intros.
-    rewrite (IntroProjProof r R t0 l a t1 k ik ik2).
-    rewrite (IndexTransProjProof r R t0 l a t1 k ik ik2). 
+    unfold isKey in ik.
+    rewrite (IntroProjProof r R t0 l a t1 k ik).
+    rewrite (IndexTransProjProof r R t0 l a t1 k ik). 
     by_extensionality g.
     by_extensionality t.
-    unfold isKey2 in ik2.
+    destruct ik as [ik ik2].
     cbn in ik2.
     apply (f_ap_eq g t) in ik2.
     cbn in ik2.
