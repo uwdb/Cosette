0d68888256cd7e6b79cfa2183dddb7b6dbd7bed5
stechu <shumo.chu@gmail.com>
2017-03-02 22:31:00 -0800

    slightly change predicate test

--- rosette/playground/test-symbolic-pred.rkt
+++ rosette/playground/test-symbolic-pred.rkt
@@ -6,8 +6,6 @@
 (define t1 (Table "t1" (list "id" "val") (gen-sym-schema 2 3)))
 (define t2 (Table "t2" (list "id") (list)))
 
-
-
 (define-symbolic p1 (~> integer? integer? boolean?)) 
 
 (define t2c (Table "t1" (list "id") (list (cons (list 0) 0))))
@@ -23,20 +21,21 @@
 (define q2
   (SELECT (VALS "t1.id")
    FROM   (NAMED t1)
-   WHERE  (F-NARY-OP = "t1.id" "t1.val"))) 
+   WHERE  (F-NARY-OP p1 "t1.id" "t1.val"))) 
 
+; expect model
 (time (verify (same q1 q2)))
 
 ;(denote-and-run q2)
 
-(define nop (F-NARY-OP > "t1.id1" "t1.id"))
+;; (define nop (F-NARY-OP > "t1.id1" "t1.id")) 
 
-(define (denote-nop f)
-  `(apply 
-      ,(filter-nary-op-f f)
-      ,(let ([val (append '(list) (map (lambda (x)
-               `(+ ,(string-length (val-column-ref-column-name x)) 1))
-            (filter-nary-op-args f)))])
-        val)))
-
+;; (define (denote-nop f)                  
+;;   `(apply 
+;;       ,(filter-nary-op-f f)
+;;       ,(let ([val (append '(list) (map (lambda (x)
+;;                `(+ ,(string-length (val-column-ref-column-name x)) 1))
+;;             (filter-nary-op-args f)))])
+;;         val)))
+;
 ;(denote-nop nop)
