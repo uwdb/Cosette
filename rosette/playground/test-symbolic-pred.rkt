a0b0b55253c33ede77854d4e82cea39b5981f428
stechu <shumo.chu@gmail.com>
2017-03-02 16:35:34 -0800

    mini example

--- rosette/playground/test-symbolic-pred.rkt
+++ rosette/playground/test-symbolic-pred.rkt
@@ -1,7 +1,7 @@
 #lang rosette
 
 (require "../cosette.rkt" "../util.rkt" "../table.rkt" 
-         "../sql.rkt" "../evaluator.rkt" "../equal.rkt" "../denotation.rkt")
+         "../sql.rkt" "../evaluator.rkt" "../equal.rkt" "../denotation.rkt" "../syntax.rkt")
 
 (define t1 (Table "t1" (list "id" "val") (gen-sym-schema 2 3)))
 (define t2 (Table "t2" (list "id") (list)))
@@ -17,7 +17,7 @@
 (define q1
   (SELECT (VALS "t1.id")
    FROM   (NAMED t2c)
-   WHERE  (F-NARY-OP > "t1.id" "t1.id"))) 
+   WHERE  (BINOP "t1.id" = "t1.id"))) 
 
 ;(define q2 (NAMED t2))
 (define q2
@@ -25,4 +25,13 @@
    FROM   (NAMED t2c)
    WHERE  (F-NARY-OP > "t1.id" "t1.id"))) 
 
-(denote-sql q1 (make-hash))
+(define nop (F-NARY-OP > "t1.id1" "t1.id"))
+
+(define (denote-nop f)
+  `(apply 
+      ,(filter-nary-op-f f)
+      ,(map (lambda (x)
+              `(+ ,(string-length (val-column-ref-column-name x)) 1))
+            (filter-nary-op-args f))))
+
+(denote-nop nop)
