831117fde116f54fdd3fca0fd2f629831e1098ed
Daniel Li <dli123@uw.edu>
2017-03-30 16:06:31 -0700

    solve upgrades

--- solve.sh
+++ solve.sh
@@ -11,8 +11,10 @@ UUID=$(head /dev/urandom | tr -dc A-Za-z | head -c 13 )
 
 
 mkdir -p hott/$BUILD
+mkdir -p .compiled/
 
 cat $FILE | $PROG > hott/$BUILD/$UUID.v
+cp hott/$BUILD/$UUID.v .compiled/
 cd hott/
 cp -n -r library $BUILD/
 cd $BUILD; (find ./library -name '*.v'; echo "./$UUID.v") | xargs coq_makefile -R . $NAME -o MakefileSolve_$UUID
