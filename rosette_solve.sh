7551a5a50d121bb34302cff56b063afa6bb63616
Daniel Li <dli123@uw.edu>
2017-04-14 00:37:18 -0700

    Rosette solver bash script.

---
+++ rosette_solve.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+PROG=/Cosette/dsl/dist/build/RosetteCodeGen/RosetteCodeGen
+NAME=Cosette
+
+BUILD=.generated
+
+FILE="$1"
+
+UUID=$(head /dev/urandom | tr -dc A-Za-z | head -c 13 )
+
+
+mkdir -p rosette/$BUILD
+mkdir -p .compiled/
+
+cat $FILE | $PROG > rosette/$BUILD/$UUID.rkt
+cp rosette/$BUILD/$UUID.rkt .compiled/
+cd rosette
+racket server.rkt rosette/$BUILD/$UUID.rkt
