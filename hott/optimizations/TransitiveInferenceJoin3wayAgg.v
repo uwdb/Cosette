1fc7d51605095136c250127eedc1a1bf61493083
stechu <shumo.chu@gmail.com>
2017-10-19 16:08:49 -0700

    prove TransitiveInferenceJoin3wayAgg (calcite)

---
+++ hott/optimizations/TransitiveInferenceJoin3wayAgg.v
@@ -0,0 +1,90 @@
+Require Import HoTT. 
+Require Import UnivalenceAxiom. 
+Require Import HoTTEx. 
+Require Import Denotation. 
+Require Import UnivalentSemantics. 
+Require Import AutoTactics. 
+Require Import CQTactics. 
+ 
+Open Scope type. 
+ 
+Module Optimization (T : Types) (S : Schemas T) (R : Relations T S)  (A : Aggregators T S). 
+  Import T S R A. 
+  Module SQL_TSRA := SQL T S R A. 
+  Import SQL_TSRA. 
+  Module AutoTac := AutoTactics T S R A. 
+  Import AutoTac. 
+  Module CQTac := CQTactics T S R A. 
+  Import CQTac. 
+ 
+  Notation combine' := combineGroupByProj.
+ 
+  Parameter count : forall {T}, aggregator T int. 
+  Notation "'COUNT' ( e )" := (aggregatorGroupByProj count e). 
+  Parameter sum : forall {T}, aggregator T int. 
+  Notation "'SUM' ( e )" := (aggregatorGroupByProj sum e). 
+  Parameter max : forall {T}, aggregator T int. 
+  Notation "'MAX' ( e )" := (aggregatorGroupByProj max e). 
+  Parameter min : forall {T}, aggregator T int. 
+  Notation "'MIN' ( e )" := (aggregatorGroupByProj min e). 
+  Parameter avg : forall {T}, aggregator T int. 
+  Notation "'AVG' ( e )" := (aggregatorGroupByProj avg e).
+ 
+  Parameter gt: Pred (node (leaf int) (leaf int)). 
+ 
+  Variable integer_1: constant int. 
+  Variable integer_7: constant int. 
+
+
+  Definition Rule: Type. 
+    refine (forall ( Γ scm_t scm_account scm_bonus scm_dept scm_emp: Schema) (rel_t: relation scm_t) (rel_account: relation scm_account) (rel_bonus: relation scm_bonus) (rel_dept: relation scm_dept) (rel_emp: relation scm_emp) (t_k0 : Column int scm_t) (t_c1 : Column int scm_t) (t_f1_a0 : Column int scm_t) (t_f2_a0 : Column int scm_t) (t_f0_c0 : Column int scm_t) (t_f1_c0 : Column int scm_t) (t_f0_c1 : Column int scm_t) (t_f1_c2 : Column int scm_t) (t_f2_c3 : Column int scm_t) (account_acctno : Column int scm_account) (account_type : Column int scm_account) (account_balance : Column int scm_account) (bonus_ename : Column int scm_bonus) (bonus_job : Column int scm_bonus) (bonus_sal : Column int scm_bonus) (bonus_comm : Column int scm_bonus) (dept_deptno : Column int scm_dept) (dept_name : Column int scm_dept) (emp_empno : Column int scm_emp) (emp_ename : Column int scm_emp) (emp_job : Column int scm_emp) (emp_mgr : Column int scm_emp) (emp_hiredate : Column int scm_emp) (emp_comm : Column int scm_emp) (emp_sal : Column int scm_emp) (emp_deptno : Column int scm_emp) (emp_slacker : Column int scm_emp), _). 
+    refine (⟦ Γ ⊢ (SELECT1 (e2p (constantExpr integer_1)) FROM1 (product ((SELECT  (combine' PLAIN(variable (right⋅emp_deptno)) (COUNT(variable (right⋅emp_slacker)))) FROM1  (table rel_emp) WHERE (castPred (combine (right⋅emp_deptno) (e2p (constantExpr integer_7)) ) gt) GROUP BY  (right⋅emp_deptno))) (product (table rel_emp) (table rel_emp))) WHERE (and (equal (variable (right⋅left⋅left)) (variable (right⋅right⋅left⋅emp_deptno))) (equal (variable (right⋅right⋅left⋅emp_deptno)) (variable (right⋅right⋅right⋅emp_deptno))))) : _ ⟧ =  ⟦ Γ ⊢ (SELECT1 (e2p (constantExpr integer_1)) FROM1 (product ((SELECT  (combine' PLAIN(variable (right⋅emp_deptno)) (COUNT(variable (right⋅emp_slacker)))) FROM1  (table rel_emp) WHERE (castPred (combine (right⋅emp_deptno) (e2p (constantExpr integer_7)) ) gt) GROUP BY  (right⋅emp_deptno))) (product ((SELECT * FROM1 (table rel_emp) WHERE (castPred (combine (right⋅emp_deptno) (e2p (constantExpr integer_7)) ) gt))) ((SELECT * FROM1 (table rel_emp) WHERE (castPred (combine (right⋅emp_deptno) (e2p (constantExpr integer_7)) ) gt))))) WHERE (and (equal (variable (right⋅left⋅left)) (variable (right⋅right⋅left⋅emp_deptno))) (equal (variable (right⋅right⋅left⋅emp_deptno)) (variable (right⋅right⋅right⋅emp_deptno))))) : _ ⟧). 
+  Defined. 
+  Arguments Rule /. 
+ 
+  Lemma ruleStand: Rule. 
+    start.
+    f_ap.
+    by_extensionality t0.
+    destruct t0 as [[i1 i2] [t1 t2]].
+    simpl.
+    rewrite (path_universe_uncurried (equiv_prod_symm _ _)).
+    symmetry.
+    rewrite (path_universe_uncurried (equiv_prod_symm _ _)).
+    repeat rewrite (path_universe_uncurried (equiv_prod_assoc _ _ _)).
+    f_ap.
+    rewrite (path_universe_uncurried (equiv_prod_symm _ _)).
+    repeat rewrite (path_universe_uncurried (equiv_prod_assoc _ _ _)).
+    rewrite (path_universe_uncurried (equiv_prod_symm _ _)).
+    symmetry.
+    rewrite (path_universe_uncurried (equiv_prod_symm _ _)).
+    f_ap.
+    apply path_universe_uncurried.
+    apply equiv_iff_hprop_uncurried.
+    constructor.
+    + intros [[[h1 h2] h3] h4].
+      strip_truncations.
+      destruct h1.
+      destruct h2.
+      destruct h3.
+      destruct h4 as [t [[e1 e2] e3]].
+      rewrite <- (path_universe_uncurried (equiv_path_prod _ _)) in e3.
+      destruct e3 as [e3 e4].
+      simpl in *.
+      destruct e3.
+      repeat constructor; try simp_solve.
+      apply tr.
+      refine (t; _).
+      repeat constructor; try simp_solve.
+      rewrite <- (path_universe_uncurried (equiv_path_prod _ _)).
+      simpl.
+      repeat constructor; try simp_solve.
+    + intros [[[[h1 h2] h3] h4] h5].
+      destruct h1 as [h1 h6].
+      destruct h6.
+      destruct h3.
+      destruct h2.
+      repeat constructor; try simp_solve.
+  Qed. 
+ 
+End Optimization. 
