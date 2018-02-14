e1e0cfd0d7b3beb9e1ee879eb43b875acaf910df
stechu <shumo.chu@gmail.com>
2018-02-14 14:07:11 -0800

    add one ac example

--- uexp/src/uexp/u_semiring.lean
+++ uexp/src/uexp/u_semiring.lean
@@ -23,6 +23,16 @@ infix `≃`:50 := ueq
 @[simp] axiom time_zero (a : usr): a * zero = zero
 
 -- these two lemmas are just to try the new encoding
+
+-- this one works, hoory!
+lemma lem_jared :
+    forall (v : Type) (e1 e2 e3 : usr),
+    (e1 + e2) * e3 = (e2 * e3) + (e3 * e1)  :=
+begin
+    intros,
+    simp,
+end
+
 lemma eq_mixed_congruence :
     forall (t₁ t₂: tuple) (R: tuple → usr),
     (t₁ ≃ t₂) * (R t₁) = (t₁ ≃ t₂) * (R t₂) :=
@@ -33,4 +43,4 @@ end
 
 lemma eq_sigma_subst:
     forall (R: tuple → usr) (t₂ t : tuple),
-    sig (λ t₁ : tuple,  (t₁ ≃ t₂) * (R t)) = (R t) := sorry
+    sig (λ t₁ : tuple,  (t₁ ≃ t₂) * (R t₁)) = (R t) := sorry
