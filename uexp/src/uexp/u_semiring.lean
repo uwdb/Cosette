d9c8570834c73dac2ca6a746d9704ab6f4e3d321
stechu <shumo.chu@gmail.com>
2018-02-13 23:56:36 -0800

    it looks like a better typing

--- uexp/src/uexp/u_semiring.lean
+++ uexp/src/uexp/u_semiring.lean
@@ -1,14 +1,17 @@
 -- definition of u-semiring
 constant usr : Type
+constant tuple : Type
 constant plus : usr → usr → usr
 constant time : usr → usr → usr
 constant zero : usr
 constant one : usr
-constant sig : (string → usr) → usr -- is that right?
--- TODO: define sig
+constant sig : (tuple → usr) → usr -- is that right?
+constant ueq : tuple → tuple → usr
 
 local infix * := time
 local infix + := plus
+
+infix `≃`:50 := ueq
 
 -- commutative semiring axioms
 axiom plus_comm (a b : usr) :       a + b = b + a
@@ -19,3 +22,11 @@ axiom time_assoc_l (a b c : usr) :    a * (b + c) = a * b + a * c
 axiom time_assoc_r (a b c : usr) :    (a + b) * c = a * c + b * c
 axiom time_zero (a : usr): a * zero = zero
 
+-- these two lemmas are just to try the new encoding
+lemma eq_mixed_congruence :
+    forall (t₁ t₂: tuple) (R: tuple → usr),
+    (t₁ ≃ t₂) * (R t₁) = (t₁ ≃ t₂) * (R t₂) := sorry 
+
+lemma eq_sigma_subst:
+    forall (R: tuple → usr) (t₂ t : tuple),
+    sig (λ t₁ : tuple,  (t₁ ≃ t₂) * (R t)) = (R t) := sorry 
