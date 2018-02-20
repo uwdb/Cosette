478b0675264065038091620e9a6f8ead7d38077e
stechu <shumo.chu@gmail.com>
2018-02-19 19:45:40 -0800

    refactor

--- uexp/src/uexp/examples.lean
+++ uexp/src/uexp/examples.lean
@@ -1,99 +1,38 @@
-import .core
+import .u_semiring
 import .tactics
 
-open uexp
+-- set_option trace.simp_lemmas.invalid true
+-- set_option trace.simplify true
 
-set_option trace.simp_lemmas.invalid true
-set_option trace.simplify true
+local infix * := time
+local infix + := plus
 
-#check uexp.eq.refl
+infix `≃`:50 := ueq
 
-lemma uexp_refl :
-    forall (v : Type) (e1 e2 : uexp v),
-    e1 ≃ e1 :=
-begin
-    intros, usimp
-end
+-- these two lemmas are just to try the new encoding
 
-lemma uexp_add_comm :
-    forall (v : Type) (e1 e2 e3 : uexp v),
-    (e1 + e2) ≃ (e2 + e1) :=
-begin
-    intros,
-    usimp,
-end
-
+-- this one works, hoory!
 lemma lem_jared :
-    forall (v : Type) (e1 e2 e3 : uexp v),
-    (e1 + e2) * e3 ≃ (e2 * e3) + (e3 * e1)  :=
+    forall (v : Type) (e1 e2 e3 : usr),
+    (e1 + e2) * e3 = (e2 * e3) + (e3 * e1)  :=
 begin
     intros,
-    usimp,
+    simp,
 end
-
-lemma eq_predicate_congruence :
-    forall (v: Type) (a b c d: uexp v),
-    (a~=b) * (b~=c) ≃ (a~=b) * (a~=c) :=
-sorry
 
 lemma eq_mixed_congruence :
-    forall (v: Type) (a b: uexp v) (r: uexp v -> uexp v),
-    (a~=b) * (r a) = (b~=a) * (r b) :=
-sorry
-
--- this requires a new axiom
-lemma sum_subst (v: Type) (t₂ : v):
-uexp.sigma (λ t₁ : v, (t₁ ~= t₂) * (rel "R" t₁)) ≃
-(rel "R" t₂) :=
-sorry
-
-lemma email_ex_refl (v : Type) :
-uexp.sigma (λ t₁ : v,
-  uexp.sigma (λ t₂ : v,
-   ((var t₁) ~= (var t₂)) *
-        uexp.sigma (λ t₃ : v,
-            (t₃=>"k" ~= t₁=>"k") *
-            (t₃=>"a" ~= t₁=>"a") *
-            (rel "R" t₃)) * (rel "R" t₂) *
-            (t₁=>"k" ~= t₂=>"k") *
-            (le (t₁=>"a") (uexp.int 12)))) ≃
-uexp.sigma (λ t₁ : v,
-  uexp.sigma (λ t₂ : v,
-   (t₁ ~= t₂) *
-        uexp.sigma (λ t₃ : v,
-            (t₃=>"k" ~= t₁=>"k") *
-            (t₃=>"a" ~= t₁=>"a") *
-            (rel "R" t₃)) * (rel "R" t₂) *
-            (t₁=>"k" ~= t₂=>"k") *
-            (le (t₁=>"a") (uexp.int 12)))) :=
-begin
-    intros, apply uexp.eq.refl,
-end
-
-lemma email_ex_full (v : Type) :
-uexp.sigma (λ t₁ : v,
-  uexp.sigma (λ t₂ : v,
-   (t₁ ~= t₂) *
-        uexp.sigma (λ t₃ : v,
-            (t₃=>"k" ~= t₁=>"k") *
-            (t₃=>"a" ~= t₁=>"a") *
-            (rel "R" t₃)) * (rel "R" t₂) *
-            (t₁=>"k" ~= t₂=>"k") *
-            (le (t₁=>"a") (uexp.int 12)))) ≃
-uexp.sigma (λ t₁ : v,
-  uexp.sigma (λ t₂ : v,
-    uexp.sigma (λ t₃ : v,
-        (t₁ ~= t₂) * (t₁=>"k" ~= t₂=>"k") *
-        (le (t₁=>"a") (uexp.int 12)) *
-        (t₃=>"k" ~= t₁=>"k") *
-        (t₃=>"a" ~= t₁=>"a") *
-        (rel "R" t₃)) * (rel "R" t₂))) :=
+    forall (t₁ t₂: tuple) (R: tuple → usr),
+    (R t₁) * (t₁ ≃ t₂)  = (t₁ ≃ t₂) * (R t₂) :=
 begin
     intros,
-    apply uexp.eq.sigma_inj; intros,
-    apply uexp.eq.sigma_inj; intros,
-    apply uexp.eq.trans; intros,
-    apply uexp.eq.sigma_inj,
-    tactic.rotate_left 1,
-    apply uexp.eq.mul_comm; intros,
+    simp,
+end
+
+-- this one breaks something
+lemma eq_sigma_subst:
+    forall (R: tuple → usr) (t : tuple),
+    sig (λ t₁ : tuple,  (t₁ ≃ t) * (R t₁)) = (R t) := 
+begin
+    intros,
+    simp,
 end
--- uexp/src/uexp/u_semiring.lean
+++ uexp/src/uexp/u_semiring.lean
@@ -56,31 +56,3 @@ infix `≃`:50 := ueq
 @[simp] axiom sig_eq (t₁ : tuple) : sig (λ t₂, t₁ ≃ t₂) = one
 @[simp] axiom eq_unique (t' : tuple):
     sig (λ t: tuple, (t ≃ t')) = one
-
--- these two lemmas are just to try the new encoding
-
--- this one works, hoory!
-lemma lem_jared :
-    forall (v : Type) (e1 e2 e3 : usr),
-    (e1 + e2) * e3 = (e2 * e3) + (e3 * e1)  :=
-begin
-    intros,
-    simp,
-end
-
-lemma eq_mixed_congruence :
-    forall (t₁ t₂: tuple) (R: tuple → usr),
-    (R t₁) * (t₁ ≃ t₂)  = (t₁ ≃ t₂) * (R t₂) :=
-begin
-    intros,
-    simp,
-end
-
--- this one breaks something
-lemma eq_sigma_subst:
-    forall (R: tuple → usr) (t : tuple),
-    sig (λ t₁ : tuple,  (t₁ ≃ t) * (R t₁)) = (R t) := 
-begin
-    intros,
-    simp,
-end
