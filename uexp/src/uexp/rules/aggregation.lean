9af446f57294d9abcaf8821eb1c9e30e87910faa
Brendan Seamas Murphy <shamrockfrost@gmail.com>
2018-03-08 04:56:30 -0800

    Add Aggregate* rules

--- uexp/src/uexp/rules/aggregation.lean
+++ uexp/src/uexp/rules/aggregation.lean
@@ -16,13 +16,13 @@ forall (Γ : Schema) s (a : relation s) C0 (value : Column C0 s) C1 (label: Colu
         lbl' : Expr (Γ ++ (leaf C1 ++ leaf datatypes.int)) C1 := uvariable (right⋅left)
     in (denoteSQL ((SELECT *
                   FROM1
-                  (SELECT (combineGroupByProj (PLAIN lbl) (COUNT val))
+                  (SELECT (combineGroupByProj PLAIN(lbl) COUNT(val))
                    FROM1 (table a)
                    GROUP BY (right⋅label))
                   WHERE equal lbl' (constantExpr l)) : SQL Γ _)) = 
        (denoteSQL ((SELECT *
                    FROM1
-                   (SELECT (combineGroupByProj (PLAIN lbl) (COUNT val))
+                   (SELECT (combineGroupByProj PLAIN(lbl) COUNT(val))
                     FROM1 (SELECT * FROM1 (table a) WHERE equal lbl (constantExpr l))
                     GROUP BY (right⋅label))) : SQL Γ _)) :=
 begin
