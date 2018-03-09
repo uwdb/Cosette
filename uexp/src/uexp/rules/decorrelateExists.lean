dbc30ecb30db65bb8aa949382271a9f2ad9d1c1c
Brendan Seamas Murphy <shamrockfrost@gmail.com>
2018-03-09 07:05:42 -0800

    Add rules C through F, except the ones for cse344

---
+++ uexp/src/uexp/rules/decorrelateExists.lean
@@ -0,0 +1,46 @@
+import ..sql
+import ..tactics
+import ..u_semiring
+import ..extra_constants
+
+open Expr
+open Proj
+open Pred
+open SQL
+
+variable i : const datatypes.int
+
+theorem rule :
+    forall (Γ scm_emp : Schema)
+           (rel_emp : relation scm_emp)
+           (emp_empno : Column datatypes.int scm_emp)
+           (emp_ename : Column datatypes.int scm_emp)
+           (emp_job : Column datatypes.int scm_emp)
+           (emp_mgr : Column datatypes.int scm_emp)
+           (emp_hiredate : Column datatypes.int scm_emp)
+           (emp_comm : Column datatypes.int scm_emp)
+           (emp_sal : Column datatypes.int scm_emp)
+           (emp_deptno : Column datatypes.int scm_emp)
+           (emp_slacker : Column datatypes.int scm_emp),
+    let left_deptno : Expr ((Γ ++ scm_emp) ++ scm_emp) datatypes.int
+                    := uvariable (left⋅right⋅emp_deptno),
+        right_deptno : Expr ((Γ ++ scm_emp) ++ scm_emp) datatypes.int
+                    := uvariable (right⋅emp_deptno) in
+    denoteSQL
+    (SELECT *
+     FROM1 (table rel_emp
+            WHERE EXISTS (SELECT *
+                           FROM1 table rel_emp
+                           WHERE (equal left_deptno right_deptno)))
+    : SQL Γ _) =
+    denoteSQL
+    (SELECT1 (right⋅left⋅star)
+     FROM1 (product (table rel_emp)
+                     (DISTINCT (SELECT1 (combine (right⋅emp_deptno)
+                                                 (e2p (constantExpr i)))
+                                FROM1 table rel_emp))
+            WHERE (equal (uvariable (right⋅left⋅emp_deptno))
+                         (uvariable (right⋅right⋅left)))) : SQL Γ _) :=
+begin
+admit
+end
---
+++ uexp/src/uexp/rules/fkPennTR.lean
@@ -0,0 +1,59 @@
+import ..sql
+import ..tactics
+import ..u_semiring
+import ..extra_constants
+
+open Expr
+open Proj
+open Pred
+open SQL
+
+variable str_security_ : const datatypes.int
+
+theorem rule :
+    forall (Γ scm_payroll scm_teams scm_depts : Schema)
+           (rel_payroll : relation scm_payroll)
+           (rel_teams : relation scm_teams)
+           (rel_depts : relation scm_depts)
+           (payroll_pdept : Column datatypes.int scm_payroll)
+           (payroll_empl : Column datatypes.int scm_payroll)
+           (teams_tproj : Column datatypes.int scm_teams)
+           (teams_tmember : Column datatypes.int scm_teams)
+           (depts_dname : Column datatypes.int scm_depts)
+           (depts_dproj : Column datatypes.int scm_depts)
+           (k : isKey payroll_empl rel_payroll)
+           (fk : fKey payroll_empl teams_tmember rel_payroll rel_teams k),
+    denoteSQL
+    (SELECT1 (right⋅right⋅teams_tmember)
+     FROM1 (product (table rel_depts) (table rel_teams))
+     WHERE (and (equal (uvariable (right⋅left⋅depts_dproj))
+                       (uvariable (right⋅right⋅teams_tproj)))
+                (equal (uvariable (right⋅left⋅depts_dname))
+                       (constantExpr str_security_))) : SQL Γ _) =
+    denoteSQL
+    (SELECT1 (right⋅left⋅right⋅right)
+     FROM1 (product ((SELECT1 (combine (right⋅left⋅depts_dname)
+                              (combine (right⋅left⋅depts_dproj)
+                                       (right⋅right⋅payroll_empl)))
+                      FROM1 (product (table rel_depts)
+                                     (table rel_payroll))
+                      WHERE (equal (uvariable (right⋅left⋅depts_dname))
+                                   (uvariable (right⋅right⋅payroll_pdept)))))
+                    ((SELECT1 (combine (right⋅left⋅teams_tmember)
+                              (combine (right⋅right⋅payroll_pdept)
+                                       (right⋅left⋅teams_tproj)))
+                      FROM1 (product (table rel_teams)
+                                     (table rel_payroll))
+                      WHERE (equal (uvariable (right⋅left⋅teams_tmember))
+                                   (uvariable (right⋅right⋅payroll_empl))))))
+     WHERE (and (and (and (equal (uvariable (right⋅left⋅left))
+                                 (constantExpr str_security_))
+                          (equal (uvariable (right⋅left⋅right⋅left))
+                                 (uvariable (right⋅right⋅right⋅right))))
+                     (equal (uvariable (right⋅left⋅right⋅right))
+                            (uvariable (right⋅right⋅left))))
+                (equal (uvariable (right⋅left⋅left))
+                       (uvariable (right⋅right⋅right⋅left)))) : SQL Γ _) :=
+begin
+admit
+end
