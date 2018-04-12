3433ed099ec6d8859072192c5d201d5e256e49c8
Chenglong <mestway@gmail.com>
2018-04-12 11:22:58 -0700

    add qex file

---
+++ qex_solve.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+NAME=Cosette
+
+BUILD=generated
+
+FILE="$1"
+
+filename=$(basename "$FILE")
+filename="${filename%.*}"
+
+UUID="tmp_"$filename
+
+mkdir -p rosette/$BUILD
+mkdir -p .compiled/
+
+cp $FILE rosette/$BUILD/$UUID.rkt
+cp rosette/$BUILD/$UUID.rkt .compiled/
+cd rosette
+
+#echo "[OK] solving "$BUILD/$UUID.rkt
+
+racket qex-test-server.rkt $BUILD/$UUID.rkt "$2" "$3" "$4"
