782a41f8849586c4f9111f3d97d03f6cce4dfcf1
clwang <clwang@cs.washington.edu>
2018-04-15 16:13:12 -0700

    aha

---
+++ rosette/micro-bench/2.rkt
@@ -0,0 +1,29 @@
+#lang rosette
+
+(current-bitwidth 10)
+
+(require "../cosette.rkt" "../util.rkt" "../denotation.rkt" 
+         "../sql.rkt" "../evaluator.rkt" "../syntax.rkt" "../symmetry.rkt") 
+
+(provide ros-instance)
+
+(define scores-info (table-info "Scores" (list "StudentID" "CourseID" "Points")))
+(define students-info (table-info "Students" (list "StudentNr" "StudentName")))
+
+; DECLARE @x as tinyint;
+; SELECT DISTINCT Scores.StudentID, SUM(Scores.Points)
+; FROM Scores
+; WHERE Scores.Points > 2
+; HAVING SUM(Scores.Points) >= @x AND @x > 5
+
+(define (q tables)
+   (SELECT-DISTINCT (VALS "t.StudentID" "t.Points")
+    FROM (AS (SELECT (VALS "Scores.StudentID" (SUM "Scores.Points"))
+              FROM   (NAMED (list-ref tables 0))
+              WHERE (BINOP "Scores.Points" > 2)
+              GROUP-BY (list)         
+              HAVING (BINOP (COUNT-DISTINCT "Scores.Points") >= 2))
+       ["t" (list "StudentID" "Points")])
+    WHERE (TRUE)))
+
+(define ros-instance (list q (list scores-info students-info) (list) prop-table-empty)) 
