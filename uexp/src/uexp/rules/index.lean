8678d7e453155ebc12680896b7c74016597f05fc
stechu <shumo.chu@gmail.com>
2018-05-11 23:11:14 -0700

    some new rules

---
+++ uexp/src/uexp/rules/index.lean
@@ -0,0 +1,56 @@
+import ..sql
+import ..tactics
+import ..u_semiring
+import ..extra_constants
+import ..ucongr
+import ..TDP
+
+set_option profiler true
+
+open Expr
+open Proj
+open Pred
+open SQL
+open tree
+
+notation `int` := datatypes.int
+
+variable integer_10: const datatypes.int
+
+lemma IndexQ0: 
+    forall Γ r (R: relation r) t0 (l: const t0)
+              (a: Column t0 r) t1 (k: Column t1 r) (ik: isKey k R),
+    denoteSQL ((SELECT * FROM1  (table R)
+                       WHERE equal (uvariable (right⋅a)) (constantExpr l)): SQL Γ _) =
+    denoteSQL ((project (right⋅right) (FROM1 (product (Index (@table Γ r R) k a) (table R))
+                       WHERE and (equal (uvariable (right⋅left⋅right⋅left⋅star)) (constantExpr l))
+                    (equal (uvariable (right⋅left⋅left⋅star)) (uvariable (right⋅right⋅k))) )) :SQL Γ _) :=
+begin
+    delta Index,
+    intros,
+    delta select2,
+    delta projectCons,
+    unfold_all_denotations,
+    funext,
+    unfold pair,
+    simp,
+    unfold isKey at ik,
+    apply ueq_symm,
+    remove_dup_sigs,
+    dsimp,
+    remove_dup_sigs,
+    dsimp,
+    remove_dup_sigs,
+    have h: (∑ (x : Tuple r),
+       (denote_c l≃denoteProj a x) * ((denoteProj k t≃denoteProj k x) * (denote_r R t * denote_r R x))) = (∑ (x : Tuple r),
+       (denote_c l≃denoteProj a x) * ((t≃x) * denote_r R t)),
+    focus{
+        apply congr_arg _,
+        funext,
+        rw (time_comm (denote_r R t)),
+        rw ik,
+    },
+    rw h,
+    remove_dup_sigs,
+    refl,
+end
---
+++ uexp/src/uexp/rules/removeSemiJoinRight.lean
@@ -0,0 +1,30 @@
+import ..sql
+import ..tactics
+import ..u_semiring
+import ..extra_constants
+import ..ucongr
+import ..TDP
+
+set_option profiler true
+
+open Expr
+open Proj
+open Pred
+open SQL
+open tree
+
+notation `int` := datatypes.int
+
+
+theorem rule:
+forall ( Γ scm_dept scm_emp: Schema) (rel_dept: relation scm_dept) (rel_emp: relation scm_emp) (dept_deptno : Column int scm_dept) (dept_name : Column int scm_dept) (emp_empno : Column int scm_emp) (emp_ename : Column int scm_emp) (emp_job : Column int scm_emp) (emp_mgr : Column int scm_emp) (emp_hiredate : Column int scm_emp) (emp_comm : Column int scm_emp) (emp_sal : Column int scm_emp) (emp_deptno : Column int scm_emp) (emp_slacker : Column int scm_emp),
+denoteSQL ((SELECT1 (right⋅left⋅emp_ename) FROM1 (product (table rel_emp) (product (table rel_dept) (table rel_emp))) WHERE (and (equal (uvariable (right⋅left⋅emp_deptno)) (uvariable (right⋅right⋅left⋅dept_deptno))) (equal (uvariable (right⋅right⋅left⋅dept_deptno)) (uvariable (right⋅right⋅right⋅emp_deptno))))) :SQL Γ _)
+=
+denoteSQL ((SELECT1 (right⋅left⋅emp_ename) FROM1 (product (table rel_emp) (product (table rel_dept) (table rel_emp))) WHERE (and (equal (uvariable (right⋅left⋅emp_deptno)) (uvariable (right⋅right⋅left⋅dept_deptno))) (equal (uvariable (right⋅right⋅left⋅dept_deptno)) (uvariable (right⋅right⋅right⋅emp_deptno))))) :SQL Γ _)  :=
+begin
+  intros,
+  unfold_all_denotations,
+  funext,
+  try {simp},
+  try {TDP' ucongr},
+end
