1710b8351b196f80321601a941fa6e1ad758f5d0
Chenglong Wang <mestway@gmail.com>
2021-01-26 22:08:05 -0800

    another docker init file

---
+++ docker_init_with_mount.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+NAME="cosette-minimal-container"
+DIR="/Cosette-Dev"
+
+echo "[0] starting container with name $NAME ..."
+
+# the command to build and test solver
+
+# uncomment the following command if not exist
+# docker build -t cosette-minimal .
+
+#echo "[0] starting container with name $NAME ..."
+docker run -v $(pwd)/:$DIR -w $DIR --name=$NAME -it cosette-minimal bash #-c "$CMD"
+
+#echo "[2] removing the container ..."
+#docker rm $NAME
+
+#echo "[3] remove generated files ..."
+#rm -r .compiled/
+#rm -r dsl/dist/
+#rm -r hott/.build_solve/
+#rm -r rosette/generated/
+#rm solver.pyc
